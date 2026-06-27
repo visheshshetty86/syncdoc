1010fb027e2e402ec679eeb7ea1ee620aa0b5aa0
vishesh <visheshshetty86@gmail.com>
2026-06-27 17:13:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Editor from "./components/Editor/Editor.jsx";
+
+export default function App() {
+  return (
+    <Editor />
+  );
+}
---
+++ src/components/Editor/Editor.jsx
@@ -0,0 +1,7 @@
+export default function Editor() {
+  return (
+    <div>
+        <h1>Editor Component</h1>
+    </div>
+  );
+}
